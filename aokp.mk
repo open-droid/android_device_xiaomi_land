3493ffe18ec209f8d9c29495ccf2f05346dbf605
Hriday Sharma <hridaysharma42@gmail.com>
2017-01-05 16:03:13 +0000

    land: AOKP bringup
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The Andorid Open Kang Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,8 +21,8 @@ $(call inherit-product, $(LOCAL_PATH)/PlatformConfig.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +30,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := cm_land
+PRODUCT_NAME := aokp_land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
