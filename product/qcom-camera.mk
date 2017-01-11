653e6a24243bf4ba74d2fd2995421893368e3a60
Hriday Sharma <hridaysharma42@gmail.com>
2017-01-11 13:43:44 +0000

    land: Build camera from source
    * Cleanup
    
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,10 +1,12 @@
 # Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false
+PRODUCT_PACKAGES += \
+     camera.$(TARGET_BOARD_PLATFORM) \
+     libmm-qcamera
 
-# ZRAM
+# Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
     media.camera.ts.monotonic=0 \
+    persist.camera.cpp.duplication=false
     persist.camera.gyro.android=1 \
     persist.camera.is_type=1
