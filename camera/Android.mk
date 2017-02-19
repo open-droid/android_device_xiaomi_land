c0ed366ad3c18bd41bf5093e07c51605fedb5bd9
Luís Ferreira <lsferreira169@gmail.com>
2017-02-19 20:04:15 +0000

    Revert: [Camera] OSS Camera HAL

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,26 +1,18 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8909
-MM_V4L2_DRIVER_LIST += msm8952
-MM_V4L2_DRIVER_LIST += msm8996
-MM_V4L2_DRIVER_LIST += msm8992
-MM_V4L2_DRIVER_LIST += msm8937
-MM_V4L2_DRIVER_LIST += titanium
-MM_V4L2_DRIVER_LIST += msmcobalt
+LOCAL_PATH := $(call my-dir)
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
-  endif
-endif
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libgui libutils
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
