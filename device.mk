5707ffa79f41ea558d21fe59db3eb2d4525c0be5
Luís Ferreira <lsferreira169@gmail.com>
2017-02-21 00:16:02 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,45 +20,7 @@ DEVICE_PATH := $(LOCAL_PATH)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-include $(LOCAL_PATH)/product/ant.mk
-include $(LOCAL_PATH)/product/audio.mk
-include $(LOCAL_PATH)/product/bluetooth-le.mk
-include $(LOCAL_PATH)/product/bluetooth.mk
-include $(LOCAL_PATH)/product/camera.mk
-include $(LOCAL_PATH)/product/consumerir.mk
-include $(LOCAL_PATH)/product/display.mk
-include $(LOCAL_PATH)/product/fingerprint.mk
-include $(LOCAL_PATH)/product/fm.mk
-include $(LOCAL_PATH)/product/gello.mk
-include $(LOCAL_PATH)/product/gps.mk
-include $(LOCAL_PATH)/product/init.mk
-include $(LOCAL_PATH)/product/keylayout.mk
-include $(LOCAL_PATH)/product/lights.mk
-include $(LOCAL_PATH)/product/media.mk
-include $(LOCAL_PATH)/product/misc.mk
-include $(LOCAL_PATH)/product/nfc.mk
-include $(LOCAL_PATH)/product/sensors.mk
-include $(LOCAL_PATH)/product/snap.mk
-include $(LOCAL_PATH)/product/touchscreen.mk
-include $(LOCAL_PATH)/product/usb.mk
-include $(LOCAL_PATH)/product/wifi.mk
-
-include $(LOCAL_PATH)/product/qcom-audio.mk
-include $(LOCAL_PATH)/product/qcom-cne.mk
-include $(LOCAL_PATH)/product/qcom-display.mk
-include $(LOCAL_PATH)/product/qcom-fm.mk
-include $(LOCAL_PATH)/product/qcom-gps.mk
-include $(LOCAL_PATH)/product/qcom-init.mk
-include $(LOCAL_PATH)/product/qcom-media.mk
-include $(LOCAL_PATH)/product/qcom-net.mk
-include $(LOCAL_PATH)/product/qcom-perf.mk
-include $(LOCAL_PATH)/product/qcom-power.mk
-include $(LOCAL_PATH)/product/qcom-radio.mk
-include $(LOCAL_PATH)/product/qcom-sensors.mk
-include $(LOCAL_PATH)/product/qcom-usb.mk
-include $(LOCAL_PATH)/product/qcom-wifi.mk
-
-include $(LOCAL_PATH)/product/cyanogen-livedisplay.mk
+include $(LOCAL_PATH)/product/*.mk
 
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/land/land-vendor.mk)
